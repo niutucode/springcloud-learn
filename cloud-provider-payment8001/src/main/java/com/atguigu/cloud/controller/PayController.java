aee65c8a2c470f64792aa1300c740219b120eec0
unknown <imchenzh@foxmail.com>
2024-03-22 14:51:49 +0800

    init

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
@@ -0,0 +1,64 @@
+package com.atguigu.cloud.controller;
+
+import com.atguigu.cloud.entities.Pay;
+import com.atguigu.cloud.entities.PayDTO;
+import com.atguigu.cloud.service.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@Slf4j
+@Tag(name = "支付微服务模块", description = "支付crud")
+public class PayController {
+    @Resource
+    private PayService payService;
+
+    @PostMapping("/pay/add")
+    @Operation(summary = "新增", description = "新增支付流水方法，json做字符串")
+    public String addPay(@RequestBody Pay pay) {
+        log.info(pay.toString());
+        int i = payService.add(pay);
+        return "成功插入记录，返回值：" + i;
+    }
+
+    @DeleteMapping("/pay/del/{id}")
+    @Operation(summary = "删除", description = "删除支付流水方法")
+    public Integer deletePay(@PathVariable("id") Integer id) {
+        return payService.delete(id);
+    }
+
+    @PutMapping("/pay/update")
+    @Operation(summary = "修改", description = "修改支付流水方法")
+    public String updatePay(@RequestBody PayDTO payDTO) {
+        Pay pay = new Pay();
+        BeanUtils.copyProperties(payDTO, pay);
+        int i = payService.update(pay);
+        return "成功更新记录，返回值：" + i;
+    }
+
+    @GetMapping("/pay/get/{id}")
+    @Operation(summary = "按照ID查流水", description = "查询支付流水方法")
+    public Pay getById(@PathVariable("id") Integer id) {
+        return payService.getById(id);
+    }
+
+    @GetMapping("/pay/all")
+    @Operation(summary = "获取所有的支付流水", description = "获取所有支付流水方法")
+    public List<PayDTO> getAll() {
+        List<PayDTO> payDTOList = new ArrayList<>();
+        List<Pay> allPay = payService.getAll();
+        allPay.forEach(pay -> {
+            PayDTO payDTO = new PayDTO();
+            BeanUtils.copyProperties(pay, payDTO);
+            payDTOList.add(payDTO);
+        });
+        return payDTOList;
+    }
+}
